19e0fb15ad4f821ac7594777c71fd12444a860b3
Alexander Naylor <alexandernaylor@outlook.com>
2025-06-29 13:10:49 -0400

    added loading spinner and scroll prompt

--- src/Projects.js
+++ src/Projects.js
@@ -1,4 +1,21 @@
+import { PulseLoader } from "react-spinners";
+import { useState, CSSProperties } from "react";
+
 function Projects(){
+    let [projectData, setProjectData] = useState(undefined)
+
+    if(projectData == undefined){
+        //Loading
+        return  <PulseLoader
+                color={'white'}
+                loading={true}
+                cssOverride={{}}
+                size={"1rem"}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            />
+    }
+
     return <p>TODO:</p>
 }
 
