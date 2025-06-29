056e32d4c140de5565212dd9f69c7a2444d94fac
Alexander Naylor <alexandernaylor@outlook.com>
2025-06-29 13:32:28 -0400

    changed favicon and added google analytics

--- src/App.js
+++ src/App.js
@@ -20,14 +20,14 @@ function App() {
             <br/>
             I'm offering a well rounded skill set and the ability to adapt to new technology stacks quickly.
           </p>
-          <div class="d-flex link-bar">
-            <a href = 'https://github.com/AlexNayl' class="social">
+          <div className="d-flex link-bar">
+            <a href = 'https://github.com/AlexNayl' className="social">
               <img src = 'https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=black&color=white'/>
             </a>
-            <a href = 'https://www.linkedin.com/in/alexnayl/' class="social">
+            <a href = 'https://www.linkedin.com/in/alexnayl/' className="social">
               <img src = "https://img.shields.io/badge/LinkedIn-blue?style=for-the-badge&logo=linkedin&logoColor=white"/>
             </a>
-            <a href="../Alexander_Naylor_Resume.pdf" class="text-white social">
+            <a href="../Alexander_Naylor_Resume.pdf" className="text-white social">
               <DownloadPDFIcon height="100%" fill='white'/>
               Download Resume
             </a>
